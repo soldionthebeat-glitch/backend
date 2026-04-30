dda228f6210c3a01c83ebb92551a8e34a721f61c
soldionthebeat-glitch <soldionthebeat@gmail.com>
2026-04-30 15:24:53 -0300

    Add files via upload

---
+++ models/model.js
@@ -0,0 +1,9 @@
+// models/User.js
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+module.exports = mongoose.model("User", UserSchema);
